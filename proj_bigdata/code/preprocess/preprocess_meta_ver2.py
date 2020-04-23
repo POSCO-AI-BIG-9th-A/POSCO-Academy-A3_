31f41e65e0b7f654fe4b54330642161e10cd877a
makdung23 <makdung23@naver.com>
2020-04-23 16:12:01 +0900

    new column 'dvd_over_income'
    new column 'dvd_over_income'
    : dvd_sales divided by income_usa

--- proj_bigdata/code/preprocess/preprocess_meta_ver2.py
+++ proj_bigdata/code/preprocess/preprocess_meta_ver2.py
@@ -10,8 +10,29 @@ df.drop(['index','mpa_rating_origin'],axis=1)
 
 
 
-#채은
 
+#채은
+def dvd_over_income() :
+    global df_meta
+    
+    # dvd_sales 추가 정제
+    for i in range(len(df_meta)) :
+        try: int(df_meta["dvd_sales"][i])
+        except :
+            if (df_meta["dvd_sales"][i]!="."):
+                if (pd.isna(df_meta["dvd_sales"][i])):
+                    df_meta.loc[i,["dvd_sales"]] = "."
+                else : 
+                    df_meta.loc[i,["dvd_sales"]] = df_meta["dvd_sales"][i].replace(",", "")
+    
+    # dvd_over_total: 
+    L=[]
+    for i in range(len(df_meta)):
+        if ((df_meta["dvd_sales"][i] != ".") & (df_meta["income_usa"][i] != ".")) :
+            L.append(int(df_meta["dvd_sales"][i]) / int(df_meta["income_usa"][i]))
+        else : L.append(".")
+    
+    df_meta["dvd_over_income"] = L
 
 
 
