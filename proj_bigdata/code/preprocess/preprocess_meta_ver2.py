1cd0f4b6ece34100d34211da92425d72e7ca6fd2
makdung23 <makdung23@naver.com>
2020-04-23 17:21:35 +0900

    dvd_over_income() function modify
    dvd_over_income() function modified
    : code by SY added

--- proj_bigdata/code/preprocess/preprocess_meta_ver2.py
+++ proj_bigdata/code/preprocess/preprocess_meta_ver2.py
@@ -11,7 +11,6 @@ df.drop(['index','mpa_rating_origin'],axis=1)
 
 
 
-#채은
 def dvd_over_income() :
     global df_meta
     
@@ -24,6 +23,32 @@ def dvd_over_income() :
                     df_meta.loc[i,["dvd_sales"]] = "."
                 else : 
                     df_meta.loc[i,["dvd_sales"]] = df_meta["dvd_sales"][i].replace(",", "")
+
+        #상엽코드 추가
+        movie_id = df_meta["movie_id"][i]
+        if movie_id == "tt2349460":
+            # Grace Unplugged
+            df_meta.loc[i,"dvd_sales"] = "3980659"
+            df_meta.loc[i,"blu_sales"] = "536454"
+            df_meta.loc[i,"total_sales"] = "4517113"
+
+        if movie_id == "tt0790804":
+            # The Comebacks
+            df_meta.loc[i,"dvd_sales"] = "9421539"
+            df_meta.loc[i,"blu_sales"] = "."
+            df_meta.loc[i,"total_sales"] = "9421539"
+
+        if movie_id == "tt0478304":
+            # The Tree of Life (2011)
+            df_meta.loc[i,"dvd_sales"] = "."
+            df_meta.loc[i,"blu_sales"] = "5535861"
+            df_meta.loc[i,"total_sales"] = "5535861"
+
+        if movie_id == "tt8688634":
+            # 21 Bridges (2019)
+            df_meta.loc[i,"dvd_sales"] = "562041"
+            df_meta.loc[i,"blu_sales"] = "981911"
+            df_meta.loc[i,"total_sales"] = "1543952"    
     
     # dvd_over_total: 
     L=[]
@@ -34,6 +59,7 @@ def dvd_over_income() :
     
     df_meta["dvd_over_income"] = L
 
+dvd_over_income()
 
 
 #경원
